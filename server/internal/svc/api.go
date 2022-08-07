a8bef0ef3ba4f460f12aa8527c5435c8d3172828
hbc <bcxxxxxx@gmail.com>
2022-08-07 22:21:03 +0800

    feat: api stub

--- server/internal/svc/api.go
+++ server/internal/svc/api.go
@@ -1,6 +1,8 @@
 package svc
 
 import (
+	"context"
+
 	v1 "github.com/b4fun/ku/protos/api/v1"
 	"github.com/go-logr/logr"
 	"google.golang.org/grpc"
@@ -22,4 +24,13 @@ type apiServer struct {
 	v1.UnimplementedAPIServiceServer
 
 	logger logr.Logger
+}
+
+func (s *apiServer) ListSessions(
+	ctx context.Context,
+	req *v1.ListSessionsRequest,
+) (*v1.ListSessionsResponse, error) {
+	rv := &v1.ListSessionsResponse{}
+
+	return rv, nil
 }
