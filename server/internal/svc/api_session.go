613c939167fa2ab6c0cd630d9fdce0635a9f63d4
hbc <bcxxxxxx@gmail.com>
2022-09-07 17:38:12 +0800

    feat: implement update session api

--- server/internal/svc/api_session.go
+++ server/internal/svc/api_session.go
@@ -22,9 +22,58 @@ func (s *apiServer) ListSessions(
 	return rv, nil
 }
 
+func (s *apiServer) validateUpdateSessionPayload(
+	session *v1.Session,
+) (*v1.Session, error) {
+	if session == nil {
+		return nil, status.Error(codes.InvalidArgument, "session is required")
+	}
+
+	if session.Id == "" {
+		return nil, status.Error(codes.InvalidArgument, "session id is required")
+	}
+
+	if session.Name == "" {
+		return nil, status.Error(codes.InvalidArgument, "session name is required")
+	}
+
+	return session, nil
+}
+
 func (s *apiServer) UpdateSession(
 	ctx context.Context,
 	req *v1.UpdateSessionRequest,
 ) (*v1.UpdateSessionResponse, error) {
-	return nil, nil
+	updatePayload, err := s.validateUpdateSessionPayload(req.Session)
+	if err != nil {
+		return nil, err
+	}
+
+	sessionToUpdate, err := s.sessionRepo.GetSessionByID(ctx, updatePayload.Id)
+	if err != nil {
+		s.logger.Error(err, "sessionRepo.GetSessionByID")
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	s.logger.V(8).Info(
+		"update session",
+		"session.id", sessionToUpdate.Id,
+		"session.nameOriginal", sessionToUpdate.Name,
+		"session.nameNew", updatePayload.Name,
+	)
+	sessionToUpdate.Name = updatePayload.Name
+
+	if err := s.sessionRepo.UpdateSession(ctx, sessionToUpdate); err != nil {
+		s.logger.Error(err, "sessionRepo.UpdateSession")
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	sessionUpdated, err := s.sessionRepo.GetSessionByID(ctx, sessionToUpdate.Id)
+	if err != nil {
+		s.logger.Error(err, "sessionRepo.GetSessionByID")
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	resp := &v1.UpdateSessionResponse{Session: sessionUpdated}
+	return resp, nil
 }
