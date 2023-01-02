4bbc88ae908b04b9709a9b88af7c5fe8f655de4c
hbc <me@hbc.rocks>
2023-01-02 12:28:56 -0800

    fix: fix build settings

--- browser-ui/vite.config.ts
+++ browser-ui/vite.config.ts
@@ -1,32 +1,27 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
-
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    wasm(),
-    react(),
-  ],
+  plugins: [wasm(), react(), topLevelAwait()],
   resolve: {
     alias: {
-      util: 'util',
-      events: 'events',
+      util: "util",
+      events: "events",
       process: "process/browser",
     },
   },
   optimizeDeps: {
     esbuildOptions: {
-      target: 'es2020',
-    }
+      target: "es2020",
+    },
   },
   build: {
     commonjsOptions: {
-      include: [
-        /node_modules/,
-      ],
+      include: [/node_modules/],
     },
-    target: 'es2020',
+    target: "es2020",
   },
-})
+});
