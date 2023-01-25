b73ea7af95e8432d9460c5d9fd1fb1af9a290b5d
hbc <bcxxxxxx@gmail.com>
2023-01-24 19:41:04 -0800

    chore: expose prql_compiler::compile

--- prql-js/src/lib.rs
+++ prql-js/src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use prql_compiler::sql::Options;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -9,6 +10,23 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn compile(prql: String) -> Option<String> {
-    return Some("hello, world!".to_string());
+pub fn compile(prql_query: &str) -> Option<String> {
+    return_or_throw(
+        prql_compiler::compile(
+            prql_query,
+            Some(Options {
+                format: true,
+                dialect: None,
+                signature_comment: false,
+            }),
+        )
+        .map_err(|e| e.composed("", prql_query, false)),
+    )
+}
+
+fn return_or_throw(result: Result<String, prql_compiler::ErrorMessages>) -> Option<String> {
+    match result {
+        Ok(sql) => Some(sql),
+        Err(e) => wasm_bindgen::throw_str(&e.to_json()),
+    }
 }
