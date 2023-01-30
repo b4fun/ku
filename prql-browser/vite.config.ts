e94272310ce491b910fa0cdca813e7d00a9928ee
hbc <bcxxxxxx@gmail.com>
2023-01-30 21:29:46 +0800

    wip

--- prql-browser/vite.config.ts
+++ prql-browser/vite.config.ts
@@ -1,9 +1,17 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa';
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [wasm(), react(), topLevelAwait()],
+  plugins: [
+    wasm(), react(), topLevelAwait(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      srcDir: 'src',
+      filename: 'sw.ts',
+    }),
+  ],
 });
