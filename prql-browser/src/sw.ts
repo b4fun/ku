a370c429549b5785bf3d30a030fb29676acb7dbc
hbc <bcxxxxxx@gmail.com>
2023-01-30 21:29:54 +0800

    Revert "wip"
    This reverts commit e94272310ce491b910fa0cdca813e7d00a9928ee.

--- prql-browser/src/sw.ts
+++
@@ -1,6 +0,0 @@
-declare let self: ServiceWorkerGlobalScope;
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING')
-    self.skipWaiting()
-})
--- prql-browser/vite.config.ts
+++ prql-browser/vite.config.ts
@@ -1,17 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA } from 'vite-plugin-pwa';
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    wasm(), react(), topLevelAwait(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      srcDir: 'src',
-      filename: 'sw.ts',
-    }),
-  ],
+  plugins: [wasm(), react(), topLevelAwait()],
 });
